a33173dacfa79b39384fb5c2b843ff3e820e13f4
Thomaz Guadagnini <thomaz@vivaldi.net>
2021-08-27 21:14:20 -0300

    Correcting POO things

--- scripts/grubbs.py
+++ scripts/grubbs.py
@@ -4,8 +4,60 @@ from scipy.stats import t
 from statistics import mean as mn
 from statistics import stdev as stdev
 
+
 class Grubbs:
-    def z_crit(self, n: int, alpha: float) -> float:
+    def __init__(self, values: list, samples: list = False, remove: bool = False, alpha: float = 0.05):
+        """Recieves a list of values used in graph and identify outliers
+
+        Parameters
+        ----------
+        values : list
+            List of values from individual values for graph/statistics
+        samples: list
+            List with the samples' names to be used as index
+        Returns
+        -------
+        list
+            List with new values removing outliers, or indexes to be removed from main table (?)
+        """
+        self.values = values
+        self.alpha = alpha
+        crit = self.z_crit(len(values), alpha)
+        results, mean, std, crit = self.z_score(values, crit)
+
+        self.mean = "Mean: {}".format(mean)
+        self.std = "Standard deviation: {}".format(std)
+        self.critical = "Critical Value(λ): {}".format(crit)
+
+        if samples:
+            results["Samples"] = samples
+        else:
+            pass
+
+        if remove:
+            rmvd = [x if x is not False and y == "No" else "-"
+                    for x, y in zip(results["Values"], results["Outlier?"])]
+            if "-" in rmvd:
+                rmvd.remove("-")
+            else:
+                pass
+
+            self.keeps = rmvd
+        else:
+            pass
+        self.results = results
+
+        if results["Outlier?"].value_counts()["Yes"] == 0:
+            self.outliers = "There are no outliers between all samples"
+        elif results["Outlier?"].value_counts()["Yes"] == 1:
+            self.outliers = "There is 1 outlier between all samples"
+        else:
+            self.outliers = "There are {} outliers between all samples" \
+                .format(results["Outlier?"].value_counts()["Yes"])
+        pass
+
+    @staticmethod
+    def z_crit(n: int, alpha: float) -> float:
         """ Gets both degress of freedom and alpha selected by user
         which is then used to get t critical value using Scipy t ppf
         Also there is a possibility to use one-tailed test to find critical value
@@ -27,7 +79,8 @@ class Grubbs:
         val_crit = num / den
         return abs(val_crit)
 
-    def z_score(self, vals: list, crit: float) -> DataFrame:
+    @staticmethod
+    def z_score(vals: list, crit: float) -> tuple:
         """ Recieves values and critical z value to be compared with
 
         Parameters
@@ -45,49 +98,5 @@ class Grubbs:
         std = stdev(vals)
         zis = [(abs(mean - v) / std) for v in vals]
         otls = ["No" if z < crit else "Yes" for z in zis]
-        results = DataFrame(data={"Values":vals, "Z-score": zis, "Outlier?":otls})
-        self.mean = "Mean: {}".format(mean)
-        self.std = "Standard deviation: {}".format(std)
-        self.critical = "Critical Value(λ): {}".format(crit)
-        return results
-
-    def test(self, values: list, samples: list = False, remove: bool = False, alpha: float = 0.05) -> str:
-        """Recieves a list of values used in graph and identify outliers
-
-        Parameters
-        ----------
-        values : list
-            List of values from individual values for graph/statistics
-        samples: list
-            List with the samples' names to be used as index
-        Returns
-        -------
-        list
-            List with new values removing outliers, or indexes to be removed from main table (?)
-        """
-        self.values = values
-        self.alpha = alpha
-        crit = self.z_crit(len(values), alpha)
-        results = self.z_score(values, crit)
-        
-        if samples: results["Samples"] = samples
-        else: pass
-
-        if remove: 
-            rmvd = [x if x is not False and y == "No" else "-" 
-                    for x, y in zip(results["Values"], results["Outlier?"])]
-            if "-" in rmvd:
-                rmvd.remove("-")
-            else: pass
-
-            self.keeps = rmvd
-        else: 
-            pass
-        self.results = results
-        
-        if results["Outlier?"].value_counts()["Yes"] == 0:
-            return "There are no outliers between all samples"
-        elif results["Outlier?"].value_counts()["Yes"] == 1:
-            return "There is 1 outlier between all samples"
-        else:
-            return "There are {} outliers between all samples".format(results["Outlier?"].value_counts()["Yes"])
+        results = DataFrame(data={"Values": vals, "Z-score": zis, "Outlier?": otls})
+        return results, mean, std, crit
